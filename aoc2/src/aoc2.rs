cfeecfc155b3bb199980165225cf9cb251c44c20
Andrei Litvin <andy314@gmail.com>
2023-12-02 14:29:46 -0500

    Prepare a bit for tracing

--- aoc2/src/aoc2.rs
+++ aoc2/src/aoc2.rs
@@ -4,6 +4,7 @@ use aoc2::{Bag, Game};
 #[global_allocator]
 static ALLOC: dhat::Alloc = dhat::Alloc;
 
+#[tracing::instrument]
 fn main() {
     #[cfg(feature = "dhat-heap")]
     let _profiler = dhat::Profiler::new_heap();
