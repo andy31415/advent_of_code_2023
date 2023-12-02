c649493cdec9d03b35529c0a49c8541ad99abb7b
Andrei Litvin <andy314@gmail.com>
2023-12-02 13:01:34 -0500

    Learned about include string and added a benchmark

---
+++ aoc_2/benches/benchmarks.rs
@@ -0,0 +1,15 @@
+use aoc::Game;
+
+fn main() {
+    // Run registered benchmarks.
+    divan::main();
+}
+
+#[divan::bench]
+fn part2() {
+   divan::black_box(include_str!("../input.txt"))
+        .split('\n')
+        .filter_map(Game::parse)
+        .map(|g| g.min_bag().power())
+        .sum::<u32>();
+}
