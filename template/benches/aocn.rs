9096f85b1036ac681a88624284a0df372f647e0a
Andrei Litvin <andy314@gmail.com>
2023-12-03 11:57:06 -0500

    Prepare for cargo generate template

--- template/benches/aocn.rs
+++ template/benches/aocn.rs
@@ -5,10 +5,10 @@ fn main() {
 
 #[divan::bench]
 fn part1() {
-    println!("TODO: bench part 1");
+    // println!("TODO: bench part 1");
 }
 
 #[divan::bench]
 fn part2() {
-    println!("TODO: bench part 2");
+    // println!("TODO: bench part 2");
 }
