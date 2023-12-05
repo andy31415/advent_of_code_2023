0ae70ca640f4b5e9ea26753c9b997d817837d972
Andrei Litvin <andy314@gmail.com>
2023-12-04 19:40:09 -0500

    Slight format

--- aoc4/src/lib.rs
+++ aoc4/src/lib.rs
@@ -3,7 +3,7 @@ use std::collections::HashSet;
 use nom::{
     bytes::complete::tag,
     character::complete::{digit1, space0, space1},
-    multi::{separated_list1},
+    multi::separated_list1,
     sequence::tuple,
     IResult, Parser,
 };
