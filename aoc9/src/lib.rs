663d89c458a725482d8f9b549f6fa182fb09ecbe
Andrei Litvin <andy314@gmail.com>
2023-12-09 10:50:55 -0500

    Remove polynomial and rstest as it is not relevant. sad

--- aoc9/src/lib.rs
+++ aoc9/src/lib.rs
@@ -1,11 +1,8 @@
-use std::iter::from_fn;
-
 use nom::{
     character::complete::{self, newline, space1},
     multi::separated_list1,
     IResult, Parser,
 };
-use rulinalg::matrix::Matrix;
 
 #[derive(Debug, PartialEq, PartialOrd, Clone)]
 struct Sequence {
@@ -62,85 +59,6 @@ fn parse_input(input: &str) -> IResult<&str, Input> {
         .parse(input)
 }
 
-#[derive(Debug, PartialEq, PartialOrd, Clone)]
-struct Polynomial {
-    coefficients: Vec<f64>, // a0, a1, ... (i.e. lowest to highest)
-}
-
-impl Polynomial {
-    pub fn evaluate(&self, x: f64) -> f64 {
-        let mut mul = 1.0;
-        let mut result = 0.0;
-        for c in self.coefficients.iter() {
-            result += *c * mul;
-            mul *= x;
-        }
-        result
-    }
-}
-
-impl From<Sequence> for Polynomial {
-    fn from(value: Sequence) -> Self {
-        let mut values = value.values.clone();
-        let mut coefficients = Vec::new();
-        while !values.iter().all(|v| *v == 0) {
-            coefficients.push(0.0);
-            values = values
-                .iter()
-                .zip(values.iter().skip(1))
-                .map(|(a, b)| b - a)
-                .collect();
-        }
-
-        // create the power matrix
-        let n = coefficients.len();
-        let m = Matrix::new(
-            n,
-            n,
-            (0..n)
-                .flat_map(|m| {
-                    let mut v = 1.0;
-                    let mut cnt = 0;
-                    let powers_of_n = move || {
-                        cnt += 1;
-                        if cnt > n {
-                            return None;
-                        }
-                        let oldv = v;
-                        v *= m as f64;
-                        Some(oldv)
-                    };
-                    from_fn(powers_of_n)
-                })
-                .collect::<Vec<_>>(),
-        );
-        dbg!(&m);
-
-        let inverse = m.inverse().expect("must be inversible");
-        dbg!(&inverse);
-        let c = Matrix::new(
-            n,
-            1,
-            value
-                .values
-                .iter()
-                .take(n)
-                .map(|x| *x as f64)
-                .collect::<Vec<_>>(),
-        );
-        dbg!(&c);
-
-        let r = inverse * c;
-        dbg!(&r);
-
-        assert_eq!(r.data().len(), n);
-
-        Polynomial {
-            coefficients: r.data().clone(),
-        }
-    }
-}
-
 pub fn part1(input: &str) -> i64 {
     let (rest, input) = parse_input(input).expect("Valid input");
     assert_eq!(rest, "");
@@ -157,40 +75,7 @@ pub fn part2(input: &str) -> i64 {
 
 #[cfg(test)]
 mod tests {
-    use rstest::rstest;
-
     use super::*;
-
-    #[rstest]
-    #[case("0 3 6 9 12 15")]
-    #[case("1 3 6 10 15 21")]
-    #[case("10 13 16 21 30 45")]
-    fn test_evaluate(#[case] data: &str) {
-        let values = parse_sequence(data).expect("valid").1.values;
-        let p: Polynomial = Sequence {
-            values: values.clone(),
-        }
-        .into();
-
-        dbg!(&p);
-        for (x, fx) in values.iter().enumerate() {
-            dbg!(x);
-            assert_eq!(p.evaluate(x as f64).round(), *fx as f64);
-        }
-    }
-
-    fn test_polynomial() {
-        let p: Polynomial = Sequence {
-            values: vec![1, 3, 6, 10, 15, 21],
-        }
-        .into();
-        assert_eq!(
-            p,
-            Polynomial {
-                coefficients: vec![1.0, 1.5, 0.5]
-            }
-        )
-    }
 
     #[test]
     fn test_part1() {
