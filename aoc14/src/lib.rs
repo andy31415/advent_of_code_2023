a58685758209b25bfe15ca842e0da6362bfd8a05
Andrei Litvin <andy314@gmail.com>
2023-12-14 22:14:35 -0500

    Swap works

--- aoc14/src/lib.rs
+++ aoc14/src/lib.rs
@@ -1,23 +1,34 @@
-#[derive(Debug, PartialEq, PartialOrd, Eq, Ord, Hash)]
-enum Cell {
+#[derive(Debug, PartialEq, PartialOrd, Eq, Ord, Hash, Copy, Clone)]
+enum Item {
     Free,
     Movable,
     Immovable,
 }
 
-#[derive(Debug, PartialEq, PartialOrd)]
-struct Map {
-    data: Vec<Vec<Cell>>,
-}
-
-impl From<char> for Cell {
+impl From<char> for Item {
     fn from(value: char) -> Self {
         match value {
-            '.' => Cell::Free,
-            'O' => Cell::Movable,
-            '#' => Cell::Immovable,
+            '.' => Item::Free,
+            'O' => Item::Movable,
+            '#' => Item::Immovable,
             _ => unreachable!(),
         }
+    }
+}
+
+#[derive(Debug, PartialEq, PartialOrd, Clone)]
+struct Map {
+    data: Vec<Vec<Item>>,
+}
+
+impl Map {
+    fn swap(&mut self, a: (usize, usize), b: (usize, usize)) {
+        let data_a = *self.data.get(a.0).and_then(|v| v.get(a.1)).expect("valid a");
+        let data_b = *self.data.get(b.0).and_then(|v| v.get(b.1)).expect("valid b");
+
+        *self.data.get_mut(a.0).and_then(|v| v.get_mut(a.1)).expect("valid b") = data_b;
+        *self.data.get_mut(b.0).and_then(|v| v.get_mut(b.1)).expect("valid a") = data_a;
+
     }
 }
 
@@ -34,14 +45,31 @@ fn parse_map(input: &str) -> Map {
 mod tests {
     use super::*;
 
+
     #[test]
-    fn map_parse() {
+    fn test_swap() {
+        let mut map = parse_map("#.O\nOOO\n..#");
+        map.swap((0,0), (2,1));
+        
+        assert_eq!(
+            map,
+        Map{
+            data: vec![
+                vec![Item::Free, Item::Free, Item::Movable],
+                vec![Item::Movable, Item::Movable, Item::Movable],
+                vec![Item::Free, Item::Immovable, Item::Immovable],
+            ],
+        });
+    }
+
+    #[test]
+    fn test_map_parse() {
         assert_eq!(parse_map("#.O\nOOO\n..#"),
         Map{
             data: vec![
-                vec![Cell::Immovable, Cell::Free, Cell::Movable],
-                vec![Cell::Movable, Cell::Movable, Cell::Movable],
-                vec![Cell::Free, Cell::Free, Cell::Immovable],
+                vec![Item::Immovable, Item::Free, Item::Movable],
+                vec![Item::Movable, Item::Movable, Item::Movable],
+                vec![Item::Free, Item::Free, Item::Immovable],
             ],
         });
     }
