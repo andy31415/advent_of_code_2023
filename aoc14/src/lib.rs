be76551374c8780aab5273cc18e71aa4132f41da
Andrei Litvin <andy314@gmail.com>
2023-12-14 22:08:24 -0500

    Added some parsing and tests

--- aoc14/src/lib.rs
+++ aoc14/src/lib.rs
@@ -5,13 +5,44 @@ enum Cell {
     Immovable,
 }
 
+#[derive(Debug, PartialEq, PartialOrd)]
+struct Map {
+    data: Vec<Vec<Cell>>,
+}
 
+impl From<char> for Cell {
+    fn from(value: char) -> Self {
+        match value {
+            '.' => Cell::Free,
+            'O' => Cell::Movable,
+            '#' => Cell::Immovable,
+            _ => unreachable!(),
+        }
+    }
+}
+
+fn parse_map(input: &str) -> Map {
+    Map {
+        data: input
+            .split('\n')
+            .map(|line| line.chars().map(|c| c.into()).collect())
+            .collect(),
+    }
+}
 
 #[cfg(test)]
 mod tests {
+    use super::*;
 
     #[test]
-    fn example() {
-        assert_eq!(2 + 2, 4);
+    fn map_parse() {
+        assert_eq!(parse_map("#.O\nOOO\n..#"),
+        Map{
+            data: vec![
+                vec![Cell::Immovable, Cell::Free, Cell::Movable],
+                vec![Cell::Movable, Cell::Movable, Cell::Movable],
+                vec![Cell::Free, Cell::Free, Cell::Immovable],
+            ],
+        });
     }
 }
