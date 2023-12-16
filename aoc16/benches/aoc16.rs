d303a51aabb86ad9f038fb48f749420d3f3a1152
Andrei Litvin <andy314@gmail.com>
2023-12-16 10:06:15 -0500

    Template gen for day 16

---
+++ aoc16/benches/aoc16.rs
@@ -0,0 +1,14 @@
+fn main() {
+    // Run registered benchmarks.
+    divan::main();
+}
+
+#[divan::bench]
+fn part1() {
+    // println!("TODO: bench part 1");
+}
+
+#[divan::bench]
+fn part2() {
+    // println!("TODO: bench part 2");
+}
