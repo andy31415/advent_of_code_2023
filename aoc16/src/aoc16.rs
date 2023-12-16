1b9c6ea202dfd50d061285bd47c7207d3386e5e0
Andrei Litvin <andy314@gmail.com>
2023-12-16 12:20:59 -0500

    Part 1 gold star

--- aoc16/src/aoc16.rs
+++ aoc16/src/aoc16.rs
@@ -7,7 +7,8 @@ fn main() {
     #[cfg(feature = "dhat-heap")]
     let _profiler = dhat::Profiler::new_heap();
 
-    println!("Part 1:");
+    let s1 = aoc16::part1(include_str!("../input.txt"));
+    println!("Part 1: {}", s1);
 
     println!("Part 2:");
 }
