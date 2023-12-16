1ab21735a0b5aa89834d0dcc7c54fcb15ec2d550
Andrei Litvin <andy314@gmail.com>
2023-12-15 20:16:03 -0500

    Simple hash

--- aoc15/src/lib.rs
+++ aoc15/src/lib.rs
@@ -1,9 +1,22 @@
 
+fn update_hash(current: u8, c: char) -> u8 {
+    let x = current as usize;
+    
+    let x = x + c as usize;
+    let x = x * 17;
+
+    (x & 0xFF) as u8
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
 
     #[test]
-    fn example() {
-        assert_eq!(2 + 2, 4);
+    fn test_hash() {
+        assert_eq!(update_hash(0, 'H'), 200);
+        assert_eq!(update_hash(200, 'A'), 153);
+        assert_eq!(update_hash(153, 'S'), 172);
+        assert_eq!(update_hash(172, 'H'), 52);
     }
 }
