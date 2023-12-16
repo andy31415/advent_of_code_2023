770616a27c8ef81ff615ad1d7b5672750d7c69ec
Andrei Litvin <andy314@gmail.com>
2023-12-15 20:37:32 -0500

    format

--- aoc15/src/lib.rs
+++ aoc15/src/lib.rs
@@ -16,10 +16,11 @@ fn hash_string(s: &str) -> u8 {
     s.chars().fold(0, |acc, c| update_hash(acc, c))
 }
 
-pub fn part1(s: &str) -> usize { 
-    s.split('\n').map(|l| l.split(',')).flatten().fold(0, |acc, s| {
-        acc + hash_string(s) as usize
-    })
+pub fn part1(s: &str) -> usize {
+    s.split('\n')
+        .map(|l| l.split(','))
+        .flatten()
+        .fold(0, |acc, s| acc + hash_string(s) as usize)
 }
 
 #[derive(Debug, PartialEq, Eq, Ord, PartialOrd, Hash)]
@@ -31,7 +32,7 @@ enum Operation {
 #[derive(Debug, PartialEq, Eq, Ord, PartialOrd, Hash)]
 struct Action<'a> {
     operation: Operation,
-    label:  &'a str,
+    label: &'a str,
 }
 
 impl<'a> From<&'a str> for Action<'a> {
@@ -44,14 +45,13 @@ impl<'a> From<&'a str> for Action<'a> {
             };
         }
         assert_eq!(value.chars().last().unwrap(), '-');
-        
+
         return Self {
             operation: Operation::Remove,
             label: &value[0..(value.len() - 1)],
-        }
+        };
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -59,16 +59,49 @@ mod tests {
 
     #[test]
     fn test_into_action() {
-        assert_eq!(Action { operation: Operation::Add(1), label: "rn"}, "rn=1".into());
-        assert_eq!(Action { operation: Operation::Remove, label: "cm"}, "cm-".into());
-        assert_eq!(Action { operation: Operation::Add(3), label: "qp"}, "qp=3".into());
-        assert_eq!(Action { operation: Operation::Remove, label: "pc"}, "pc-".into());
-        assert_eq!(Action { operation: Operation::Add(7), label: "ot"}, "ot=7".into());
+        assert_eq!(
+            Action {
+                operation: Operation::Add(1),
+                label: "rn"
+            },
+            "rn=1".into()
+        );
+        assert_eq!(
+            Action {
+                operation: Operation::Remove,
+                label: "cm"
+            },
+            "cm-".into()
+        );
+        assert_eq!(
+            Action {
+                operation: Operation::Add(3),
+                label: "qp"
+            },
+            "qp=3".into()
+        );
+        assert_eq!(
+            Action {
+                operation: Operation::Remove,
+                label: "pc"
+            },
+            "pc-".into()
+        );
+        assert_eq!(
+            Action {
+                operation: Operation::Add(7),
+                label: "ot"
+            },
+            "ot=7".into()
+        );
     }
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1("rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"), 1320);
+        assert_eq!(
+            part1("rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"),
+            1320
+        );
     }
 
     #[test_log::test]
@@ -86,4 +119,3 @@ mod tests {
         assert_eq!(hash_string("cm-"), 253);
     }
 }
-
