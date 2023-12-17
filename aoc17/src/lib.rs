d96d891ef22ef1b8d2a9c97d39f43bcff203e9e2
Andrei Litvin <andy314@gmail.com>
2023-12-17 12:21:02 -0500

    Prepare input

--- aoc17/src/lib.rs
+++ aoc17/src/lib.rs
@@ -1,6 +1,11 @@
 use ndarray::{Array, Array2};
 use tracing::trace;
 
+#[derive(Debug, PartialEq)]
+struct Solver {
+    values: Array2<usize>,
+}
+
 fn parse_input(input: &str) -> Array2<usize> {
     let lines = input.split('\n').collect::<Vec<_>>();
 
@@ -27,6 +32,7 @@ fn parse_input(input: &str) -> Array2<usize> {
 }
 
 pub fn part1(input: &str) -> usize {
+    let solver = Solver{values: parse_input(input)};
     // TODO: implement
     0
 }
@@ -52,11 +58,11 @@ mod tests {
 
     #[test]
     fn test_part1() {
-        assert_eq!(part1(include_str!("../input.txt")), 0);
+        assert_eq!(part1(include_str!("../example.txt")), 102);
     }
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(include_str!("../input.txt")), 0);
+        assert_eq!(part2(include_str!("../example.txt")), 0);
     }
 }
