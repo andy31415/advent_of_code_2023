d459a2e28ae4030fdae4aef536ea32dafa6bc29e
Andrei Litvin <andy314@gmail.com>
2023-12-17 13:28:35 -0500

    Dijkstra outputs something

--- aoc17/src/lib.rs
+++ aoc17/src/lib.rs
@@ -1,11 +1,11 @@
-use std::{ops::Range, iter};
+use std::panic::Location;
 
 use ndarray::{Array, Array2};
 use pathfinding::directed::dijkstra::dijkstra;
-use tracing::trace;
+use tracing::{info, trace};
 
 /// in what direction are you NOT allowed to go
-#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, Copy)]
 enum Direction {
     Left,
     Right,
@@ -14,7 +14,7 @@ enum Direction {
 }
 
 /// A location in a solution
-#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, Copy)]
 struct SolveLocation {
     deny: Direction,
     row: i32,
@@ -27,23 +27,95 @@ struct Solver {
 }
 
 impl Solver {
-    
-    fn successors(&self, pos: &SolveLocation) -> Vec<(SolveLocation, usize)> {
-        // TODO: grab weight + solve
-        // 
-        vec![]
+    fn is_inside(&self, pos: (i32, i32)) -> bool {
+        let d = self.values.dim();
+        pos.0 >= 0 && pos.1 >= 0 && (pos.0 as usize) < d.0 && (pos.1 as usize) < d.1
     }
-    
+
+    // Return the next position in this direction
+    fn next(&self, pos: (i32, i32), direction: Direction) -> Option<(i32, i32, usize)> {
+        let delta = match direction {
+            Direction::Left => (0, -1),
+            Direction::Right => (0, 1),
+            Direction::Up => (-1, 0),
+            Direction::Down => (1, 0),
+        };
+        let next = (pos.0 + delta.0, pos.1 + delta.1);
+        if self.is_inside(next) {
+            Some((
+                next.0,
+                next.1,
+                *self
+                    .values
+                    .get((next.0 as usize, next.1 as usize))
+                    .expect("inside") as usize,
+            ))
+        } else {
+            None
+        }
+    }
+
+    fn successors(&self, pos: &SolveLocation) -> Vec<(SolveLocation, usize)> {
+        let mut result = Vec::with_capacity(9);
+
+        for direction in [
+            Direction::Left,
+            Direction::Right,
+            Direction::Up,
+            Direction::Down,
+        ] {
+            if pos.deny == direction {
+                continue;
+            }
+
+            let choices = [0, 1, 2]
+                .iter()
+                .scan(Some((pos.row, pos.col, 0)), |state, _| {
+                    let state = match state {
+                        Some(v) => v,
+                        None => return None,
+                    };
+                    let next = self
+                        .next((state.0, state.1), direction)
+                        .map(|(r, c, s)| (r, c, state.2 + s));
+                    if let Some(v) = next {
+                        *state = v;
+                    }
+                    next
+                })
+                .collect::<Vec<_>>();
+
+            trace!("Choices [{:?} + {:?}]: {:?}", pos, direction, &choices);
+
+            for c in choices {
+                result.push((
+                    SolveLocation {
+                        deny: direction,
+                        row: c.0,
+                        col: c.1,
+                    },
+                    c.2,
+                ))
+            }
+        }
+
+        // TODO: grab weight + solve
+        //
+        result
+    }
+
     fn shortest_path(&self, pos: SolveLocation, goal: (usize, usize)) -> usize {
         let (target_row, target_col) = (goal.0 as i32, goal.1 as i32);
+
+        info!("Shortest path compute...");
 
         // start with a particular location and try to reach the goal
         let result = dijkstra(
             &pos,
             |p| self.successors(p),
-            |p| (p.row == target_row && p.col == target_col)
+            |p| (p.row == target_row && p.col == target_col),
         );
-        
+
         result.expect("Dijkstra finds a solution").1
     }
 }
@@ -78,13 +150,30 @@ pub fn part1(input: &str) -> usize {
         values: parse_input(input),
     };
 
-    let a = solver.shortest_path(SolveLocation{ deny: Direction::Up, row: -1, col: 0}, solver.values.dim());
-    let b = solver.shortest_path(SolveLocation{ deny: Direction::Left, row: 0, col: -1}, solver.values.dim());
-    
+    let d = solver.values.dim();
+    let goal = (d.0 - 1, d.1 - 1);
+
+    let a = solver.shortest_path(
+        SolveLocation {
+            deny: Direction::Up,
+            row: -1,
+            col: 0,
+        },
+        goal,
+    );
+    let b = solver.shortest_path(
+        SolveLocation {
+            deny: Direction::Left,
+            row: 0,
+            col: -1,
+        },
+        goal,
+    );
+
     *[a, b].iter().min().expect("have values")
 }
 
-pub fn part2(input: &str) -> usize {
+pub fn part2(_input: &str) -> usize {
     // TODO: implement
     0
 }
@@ -103,7 +192,7 @@ mod tests {
         );
     }
 
-    #[test]
+    #[test_log::test]
     fn test_part1() {
         assert_eq!(part1(include_str!("../example.txt")), 102);
     }
